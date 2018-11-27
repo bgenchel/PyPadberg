64af8ea5bf25426938a625a4e5d2fd9e2ee33e5e
Richard Savery <richard@richardsavery.com>
2018-11-27 14:11:41 -0500

    update synth with pygame for realtime play

--- synth.py
+++ synth.py
@@ -1,5 +1,7 @@
 import librosa as lb
 import numpy as np
+import pygame as pygame
+pygame.init()
 
 one, sr = lb.load('audio/1.wav') ## loads audio file
 two, sr = lb.load('audio/2.wav') ## loads audio file
@@ -7,12 +9,14 @@ three, sr = lb.load('audio/3.wav') ## loads audio file
 
 
 #Frequencies in HZ, durations in ms, soundtype is one,two,three
-def synth(frequencies, durations, soundtype):
+def synth(frequencies, durations, soundtype, canon):
 
     pitches = lb.core.hz_to_midi(frequencies) % 60
 
     durationMod = [x % 8 for x in durations]
-    
+
+    canonStart = sum(durationMod[0:2])
+
     new_sample = soundtype
     new_phrase = []
     new_sample_shift = []
@@ -23,3 +27,11 @@ def synth(frequencies, durations, soundtype):
         new_phrase = np.append(new_phrase, new_sample_shift)
 
     lb.output.write_wav('one.wav', new_phrase, sr, norm=False) #
+    pygame.mixer.music.load("one.wav")
+    pygame.mixer.music.play()
+
+    if canon is True:
+        pygame.time.Clock().tick(canonStart)
+        pygame.mixer.music.play()
+    
+    pygame.event.wait()

a5d23f7d786387a260ee473dbd0f7297682c3d76
Richard Savery <richard@richardsavery.com>
2018-11-28 23:38:45 -0500

    librosa wav repeats

--- synth.py
+++ synth.py
@@ -1,15 +1,5 @@
 import librosa as lb
 import numpy as np
-<<<<<<< HEAD
-=======
-import subprocess
-import pygame
-from pydub import AudioSegment
-from pydub.playback import play
-
-pygame.init()
-pygame.mixer.init()
->>>>>>> ba6d42907d9c4682b31f47c5dcb041243939b7bd
 
 one, sr = lb.load('audio/1.wav') ## loads audio file
 two, sr = lb.load('audio/2.wav') ## loads audio file
@@ -21,7 +11,7 @@ soundtypes =  {
     "three": three
 }
 
-#Frequencies in HZ, durations in ms, soundtype is one,two,three
+
 def synth(frequencies, durations, soundtype, canon):
 
     pitches = lb.core.hz_to_midi(frequencies) % 60
@@ -42,7 +32,6 @@ def synth(frequencies, durations, soundtype, canon):
         new_sample_shift = lb.effects.time_stretch(new_sample_shift, l)
         new_phrase = np.append(new_phrase, new_sample_shift)
 
-<<<<<<< HEAD
     if canon == 2:
         new_phrase_two = np.insert(new_phrase,0,np.zeros(int(canonStart*11025.0)))
 
@@ -51,27 +40,6 @@ def synth(frequencies, durations, soundtype, canon):
 
     if canon == 4:
         new_phrase_four = np.insert(new_phrase,0,np.zeros(int(canonStart*11025.0*3)))
-=======
-    lb.output.write_wav('temp.wav', new_phrase, sr, norm=False) #
-    subprocess.call(['play', 'temp.wav'], stdout=open("output.txt"))
-    # subprocess.Popen(['play', 'temp.wav']) 
-    # sound = AudioSegment.from_file("temp.wav", format="wav")
-    # play(sound)
-    # pygame.mixer.music.load("temp.wav")
-    # pygame.mixer.music.play(0)
-
-    if canon >= 2:
-        pygame.time.Clock().tick(canonStart)
-        pygame.mixer.music.play(0)
-
-    if canon >= 3:
-        pygame.time.Clock().tick(canonStart*2)
-        pygame.mixer.music.play(0)
-
-    if canon >= 4:
-        pygame.time.Clock().tick(canonStart*3)
-        pygame.mixer.music.play(0)
->>>>>>> ba6d42907d9c4682b31f47c5dcb041243939b7bd
 
     lb.output.write_wav('temp.wav', new_phrase, sr, norm=False) #
     lb.output.write_wav('temp2.wav', new_phrase_two, sr, norm=False)
